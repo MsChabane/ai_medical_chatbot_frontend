8afc0559560d130d080c98ef45af5f0ad55fd10d
Ms.Chabane <157377453+MsChabane@users.noreply.github.com>
2026-06-10 13:54:33 +0100

    Update API_BASE_URL to new endpoint

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,4 @@
 
 
 export const MAX_RECORDING_SECONDS =import.meta.env.VITE_MAX_RECORDING_SECONDS||180 ;
-export const API_BASE_URL = import.meta.env.VITE_API_URL||"https://chatimagecors.onrender.com"
+export const API_BASE_URL = import.meta.env.VITE_API_URL||"https://mypresc-chatbot.onrender.com"
